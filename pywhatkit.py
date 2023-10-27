4f73da3684fc4f6c41826222f1eb99ddb723e544
sahithya456 <142912609+sahithya456@users.noreply.github.com>
2023-10-27 15:32:55 +0530

    Add files via upload

---
+++ pywhatkit.py
@@ -0,0 +1,11 @@
+import pywhatkit as py
+txt=str(input("enter the text that you want to send in whatsapp"))
+
+for i in range(2,4):
+
+    py.sendwhatmsg("+919391673288",txt,13,32)
+    py.playonyt("java script")
+    py.search("who is father of python")
+
+
+
